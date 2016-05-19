3e8d53f807c17a5ae1234ca8b9e063184b1117cc
magicDGS <daniel.gomez.sanchez@hotmail.es>
2016-05-19 09:27:52 +0200

    adding more methods

--- src/main/java/org/broadinstitute/hellbender/cmdline/argumentcollections/ReadFilterArgumentCollection.java
+++ src/main/java/org/broadinstitute/hellbender/cmdline/argumentcollections/ReadFilterArgumentCollection.java
@@ -3,7 +3,9 @@ package org.broadinstitute.hellbender.cmdline.argumentcollections;
 import org.broadinstitute.hellbender.cmdline.Argument;
 import org.broadinstitute.hellbender.cmdline.ArgumentCollectionDefinition;
 import org.broadinstitute.hellbender.engine.filters.CountingReadFilter;
+import org.broadinstitute.hellbender.engine.filters.ReadFilter;
 import org.broadinstitute.hellbender.engine.filters.ReadFilterLibrary;
+import org.broadinstitute.hellbender.utils.Utils;
 
 /**
  * Argument collection for tools that requires a
@@ -23,11 +25,61 @@ public class ReadFilterArgumentCollection implements ArgumentCollectionDefinitio
     private CountingReadFilter filter;
 
     /**
+     * Set a filter for this collection
+     */
+    public void setFilter(final CountingReadFilter filter) {
+        Utils.nonNull(filter);
+        this.filter = filter;
+    }
+
+    /**
+     * Include a filter with and operation with previous ones
+     */
+    public void addAndFilter(final String name, final ReadFilter filter) {
+        Utils.nonNull(name);
+        Utils.nonNull(filter);
+        addAndFilter(new CountingReadFilter(name, filter));
+    }
+
+    /**
+     * Include a filter with and operation with previous ones
+     */
+    public void addAndFilter(final CountingReadFilter filter) {
+        Utils.nonNull(filter);
+        if (this.filter == null) {
+            this.filter = filter;
+        } else {
+            this.filter = this.filter.and(filter);
+        }
+    }
+
+    /**
+     * Includes a filter with or operation with previous ones
+     */
+    public void addOrFilter(final String name, final ReadFilter filter) {
+        Utils.nonNull(name);
+        Utils.nonNull(filter);
+        addOrFilter(new CountingReadFilter(name, filter));
+    }
+
+    /**
+     * Includes a filter with or operation with previous ones
+     */
+    public void addOrFilter(final CountingReadFilter filter) {
+        Utils.nonNull(filter);
+        if (this.filter == null) {
+            this.filter = filter;
+        } else {
+            this.filter = this.filter.or(filter);
+        }
+    }
+
+    /**
      * Returns the read filter (simple or composite) that will be applied to the reads
      *
      * Multiple filters can be composed by using {@link org.broadinstitute.hellbender.engine.filters.ReadFilter} composition methods.
      */
     public CountingReadFilter makeReadFilter(){
-        return disableAllReadFilters ? NO_FILTER : filter;
+        return (disableAllReadFilters || filter == null) ? NO_FILTER : filter;
     }
 }
